95848d2e89731c220c68e4dbd3a560f2aefb879c
Viktor Senderov <vsenderov@gmail.com>
2024-03-12 14:29:42 +0100

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qtbirds_package",
-    version="1.0.0",
+    version="1.1.0",
     description="Prepares TreePPL data",
     author="Viktor Senderov and Jan Kudlicka",
     author_email="vsenderov@gmail.com",
