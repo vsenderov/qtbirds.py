034cd836cb0da8b38bbb9e759b87d584fdb71f41
Viktor Senderov <vsenderov@gmail.com>
2024-03-12 14:24:44 +0100

    Support for reading in realistic nucleotide sequences with gaps and capital as well as small letters.

--- qtbirds/QTTree.py
+++ qtbirds/QTTree.py
@@ -1,5 +1,7 @@
 import treeppl
 
+import json
+
 @treeppl.constructor("QTNode")
 class QTNode(treeppl.Tree.Node):
     def __init__(self, *args, **kwargs):
@@ -9,6 +11,12 @@ class QTNode(treeppl.Tree.Node):
     def __repr__(self):
         return f"QTNode(left={self.left!r}, right={self.right!r}, age={self.age})"
 
+    def to_dict(self):
+        return {"type": "QTNode", "age": self.age, "left": self.left.to_dict() if self.left else None, "right": self.right.to_dict() if self.right else None}
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=4)
+
 @treeppl.constructor("QTLeaf")
 class QTLeaf(treeppl.Tree.Leaf):
     def __init__(self, *args, **kwargs):
@@ -16,7 +24,7 @@ class QTLeaf(treeppl.Tree.Leaf):
         self.age = float(self.age)
         self.index = int(self.index)
         self.sequence = str(self.sequence)
-        self.stateSequence = [int(item) for item in self.stateSequence]
+        self.stateSequence = self.construct_state_sequence(self.sequence)
         self.character = str(self.character)
         self.characterState = int(self.characterState)
 
@@ -24,3 +32,14 @@ class QTLeaf(treeppl.Tree.Leaf):
         return (f"QTNode(age={self.age}, index={self.index}, sequence={self.sequence}, "
                 f"stateSequence={self.stateSequence}, character={self.character}, "
                 f"characterState={self.characterState})")
+
+    def construct_state_sequence(self, sequence):
+        mapping = {'t': 0, 'T': 0, 'c': 1, 'C': 1, 'a': 2, 'A': 2, 'g': 3, 'G': 3, '-': -1} # Or? '-': None
+        return [mapping.get(letter, -1) for letter in sequence]
+
+    def to_dict(self):
+        return {"type": "QTLeaf", "age": self.age, "index": self.index, "sequence": self.sequence, "stateSequence": self.stateSequence, "character": self.character, "characterState": self.characterState}
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=4)
+
